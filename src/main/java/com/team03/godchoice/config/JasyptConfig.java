512f82158aaef37a0a326d2315cf42cb2b3371e9
Jeongdonghun <arrrrr180@gmail.com>
2022-12-08 18:26:25 +0900

    Fix : 키 주석처리

--- src/main/java/com/team03/godchoice/config/JasyptConfig.java
+++ src/main/java/com/team03/godchoice/config/JasyptConfig.java
@@ -10,6 +10,8 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class JasyptConfig {
 
+//    @Value("${jasypt.secret.key}")
+//    private String key;
     //test
     @Bean(name = "jasyptStringEncryptor")
     public StringEncryptor stringEncryptor() {

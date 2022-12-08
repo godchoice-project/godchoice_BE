c952a1ef0c752e5dd44f870127fa26725ae40615
Jeongdonghun <arrrrr180@gmail.com>
2022-12-08 17:54:00 +0900

    Fix : yml

--- scripts/start.sh
+++ scripts/start.sh
@@ -13,6 +13,8 @@ TIME_NOW=$(date +%c)
 echo "$TIME_NOW > $JAR_FILE 파일 복사" >> $DEPLOY_LOG
 cp $PROJECT_ROOT/build/libs/*.jar $JAR_FILE
 
+cd app/
+
 # jar 파일 실행
 echo "$TIME_NOW > $JAR_FILE 파일 실행" >> $DEPLOY_LOG
 nohup java -jar $JAR_FILE > $APP_LOG 2> $ERROR_LOG &
